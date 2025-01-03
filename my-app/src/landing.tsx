4f0e196be63abb0263c58f22073304ad26b26e50
JosephMaltese <josephmaltese34@gmail.com>
2025-01-03 14:54:24 -0500

    Added accomodation info and new favicon

--- my-app/src/landing.tsx
+++ my-app/src/landing.tsx
@@ -102,19 +102,35 @@ function Landing() {
           <h2 className="sectionHeader">{language === 'english' ? 'Accommodations' : 'Hébergement'}</h2>
 
           <div>
+            <p style={{paddingRight: '10%', paddingLeft: '10%', fontSize: '1rem', textAlign: 'start'}}>{language === 'english' ? 'We have arranged for multiple shuttles (before the ceremony & at the end of the night) to bring guests to and from the venue on our wedding day. The shuttle will depart and arrive at a central point in Vieux Quebec, which is marked on the map below. Our suggestion is to rent a hotel or AirBNB near the bus pick-up/drop-off location, if you would like to use the shuttle. There will also be parking at the venue should you decide to drive there. The venue is a 15 minutes drive away from Vieux Quebec.' : 'Il y aura un service d’autobus qui fera plusieurs allers-retours la journée du mariage, du Vieux Québec vers l’Espace Saint-Grégoire et vice-versa. L’autobus partira et arrivera à un point central dans le Vieux-Québec, voir la carte ci-dessous pour la location précise. Nous suggérons de louer un hôtel ou un AirBnB près du point de ramassage et de dépôt de l’autobus, si vous désirez utiliser l’autobus pendant la journée. Il y aura également un stationnement à la salle de réception si vous prenez la décision de conduire. La salle de réception est à environ 15 minutes du Vieux Québec. '}</p>
+            <p style={{paddingRight: '10%', paddingLeft: '10%', fontSize: '1rem', textAlign: 'start'}}>{language === 'english' ? 'There are various hotels and AirBNBs in the area at different price points. Below is a list of recommended hotels in the area, however there are countless other options not listed below.' : 'Il y a une grande variété d’hôtels et d’AirBnB dans ce coin central du Vieux-Québec, à des prix différents. Il y a une liste ci-dessous d’hôtels recommandés près du point de ramassage et dépôt de l’autobus. Il y a également de nombreuses autres options disponibles si vous préférez faire vos recherches.'}</p>
+            <p style={{paddingRight: '10%', paddingLeft: '10%', fontSize: '1rem', textAlign: 'start'}}>{language === 'english' ? 'Note: There are no blocks of rooms reserved specifically for our wedding, given the wide variety of hotels and AirBNBs in the area. We strongly suggest booking your accommodation before April, as there is a popular music festival in Vieux Quebec on the July 12th weekend.' : 'À noter : Il n’y aura pas de blocs de chambres réservés à un hôtel spécifiquement pour le mariage, vu la grande variété d’options qui s’offrent aux invités dans ce coin du Vieux Québec. Nous suggérons fortement de réserver votre logis avant avril, comme le Festival d’été de Québec se déroule le même weekend que le mariage.'}</p>
+            <p style={{paddingRight: '10%', paddingLeft: '10%', fontSize: '1rem', textAlign: 'center'}}>{language === 'english' ? 'Shuttle Pickup/Drop-off Location: 2 Rue Pierre-Olivier-Chauveau, Québec, QC G1R 0C5' : 'Point de ramassage et de dépôt de l’autobus: 2 Rue Pierre-Olivier-Chauveau, Québec, QC G1R 0C5'}</p>
+            <p style={{paddingRight: '10%', paddingLeft: '10%', fontSize: '1rem', textAlign: 'center'}}>{language === 'english' ? 'Shuttle Pickup times (afternoon) :' : 'Heures de ramassage de l’autobus (après-midi) : '}</p>
+            <p style={{paddingRight: '10%', paddingLeft: '10%', fontSize: '1rem', textAlign: 'center'}}>{language === 'english' ? 'Shuttle Drop-off times (night) :' : 'Heures de dépôt de l’autobus (nuit) :'}</p>
             <div>
-              <h3 className = "mainpageSubheading">Hotel Option 1</h3>
-              <p>Details</p>
+              <a target='_blank' style={{textDecoration: 'None'}} href="https://monsieurjean.ca/en/"><h3 className = "mainpageSubheading">Monsieur Jean</h3></a>
+              <p style={{maxWidth: '80%', fontSize: '1rem'}}>2 Rue Pierre-Olivier-Chauveau, Québec, QC G1R 0C5</p>
             </div>
 
             <div>
-              <h3 className = "mainpageSubheading">Hotel Option 2</h3>
-              <p>Details</p>
+              <a target='_blank' style={{textDecoration: 'None'}} href="https://manoir-victoria.com/index.php/en/"><h3 className = "mainpageSubheading">Hôtel Manoir Victoria</h3></a>
+              <p style={{maxWidth: '80%', fontSize: '1rem'}}>44 Côte du Palais, Québec, QC G1R 4H8</p>
             </div>
 
             <div>
-              <h3 className = "mainpageSubheading">Hotel Option 3</h3>
-              <p>Details</p>
+              <a target='_blank' style={{textDecoration: 'None'}} href="https://hotelchamplain.ca/en/"><h3 className = "mainpageSubheading">Hôtel Champlain Vieux Québec</h3></a>
+              <p style={{maxWidth: '80%', fontSize: '1rem'}}>115 Rue Sainte-Anne Rue, Québec, QC G1R 3X6</p>
+            </div>
+
+            <div>
+              <a target='_blank' style={{textDecoration: 'None'}} href="https://hotelclarendon.com/en/"><h3 className = "mainpageSubheading">Clarendon Hotel</h3></a>
+              <p style={{maxWidth: '80%', fontSize: '1rem'}}>57 Rue Sainte-Anne, Québec, QC G1R 3X4</p>
+            </div>
+
+            <div>
+              <a target='_blank' style={{textDecoration: 'None'}} href="https://leslofts.ca/public/fr"><h3 className = "mainpageSubheading">Les Lofts 1048</h3></a>
+              <p style={{maxWidth: '80%', fontSize: '1rem'}}>1048 Rue Saint-Jean, Québec, QC G1R 1R6</p>
             </div>
             
           </div>
